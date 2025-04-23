be485d7ddc76ce829e6f5b40c89897af56c4125c
Kayy <157222013+Karabs2006@users.noreply.github.com>
2025-04-23 15:36:50 +0200

    Updated Test Script

--- Test/Assets/Test.cs
+++ Test/Assets/Test.cs
@@ -5,6 +5,9 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+        int num = 1;
+        int num2 = num++;
         
     }
 
